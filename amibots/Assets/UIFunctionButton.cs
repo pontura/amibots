e41ce17e6c21076f3cf44aa84ad00dfccc567f02
pontura <pontura@gmail.com>
2017-08-16 20:37:01 -0300

    drags

---
+++ amibots/Assets/UIFunctionButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctionButton : MonoBehaviour {
+
+    public string className;
+
+	void Start () {
+		
+	}
+    public void PointerDown()
+    {
+        print("done " + className);
+    }
+    public void PointerUp()
+    {
+        print("up " + className);
+    }
+    public void PointerExit()
+    {
+        print("exit " + className);
+    }
+}
---
+++ amibots/Assets/UIFunctionSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctionSlot : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	public void OnOver () {
+        print("Over");
+	}
+    public void OnExit()
+    {
+        print("exit");
+    }
+}
